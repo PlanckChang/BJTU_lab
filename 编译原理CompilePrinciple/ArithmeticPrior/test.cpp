19111aa3f41a28ee101b47567248cf6ddbb4242e
PlanckChang <planckchang@qq.com>
2023-01-14 19:28:47 +0800

    first commit

---
+++ 编译原理CompilePrinciple/ArithmeticPrior/test.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Planck Chang on 2022/11/21.
+//
+
+#include <map>
+#include <iostream>
+using namespace std;
+int main(){
+    map<int, int> test;
+    cout << test.count(0) << endl;
+    system("pause");
+}
